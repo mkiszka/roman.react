20be5c39782b6cc0e57cc07878cc4150ed8d967a
mkiszka <github@kiszka.name>
2022-06-25 13:13:40 +0200

    100 -> C

--- src/__tests__/converters/arabicToRoman.js
+++ src/__tests__/converters/arabicToRoman.js
@@ -5,7 +5,8 @@ describe('arabicToRoman', () => {
         [1,"I"],
         [5,"V"],
         [10,"X"],
-        [50,"L"]
+        [50,"L"],
+        [100,"C"]
 
     ])('converts %s to %d', (arabic, roman) => {
         expect(arabicToRoman(arabic)).toEqual(roman);
