59d830ad013fcb55534a9fb38ce3b006acd764ea
mkiszka <github@kiszka.name>
2022-06-25 15:05:36 +0200

    Refeactored arabicToRoman

--- src/converters/arabicToRoman.js
+++ src/converters/arabicToRoman.js
@@ -24,43 +24,14 @@ function arabicToRoman(arabic) {
     let roman = "none";
 
     if (floor === 1) {
-        if (divisor === 1) {
-            roman = "I";
-        } else if (divisor === 5) {
-            roman = "V";
-        } else if (divisor === 10) {
-            roman = "X";
-        } else if (divisor === 50) {
-            roman = "L";
-        } else if (divisor === 100) {
-            roman = "C";
-        } else if (divisor === 500) {
-            roman = "D";
-        } else if (divisor === 1000) {
-            roman = "M";
-        }
+        roman = basicArabicToRoman(divisor);        
         if( mod > 0 ) {
             roman += arabicToRoman(mod);
         }
         return roman;
     } 
     if (floor === 0 && mod > 0) {
-        if (mod === 1) {
-            roman = "I";
-        } else if (mod === 5) {
-            roman = "V";
-        } else if (mod === 10) {
-            roman = "X";
-        } else if (mod === 50) {
-            roman = "L";
-        } else if (mod === 100) {
-            roman = "C";
-        } else if (mod === 500) {
-            roman = "D";
-        } else if (mod === 1000) {
-            roman = "M";
-        }
-       
+        roman = basicArabicToRoman(mod);                     
         return roman;
         
     }
