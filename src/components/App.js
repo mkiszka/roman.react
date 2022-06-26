2d22b04b95c4e20feb5685ec327ceddf1452fc22
mkiszka <github@kiszka.name>
2022-06-26 12:16:11 +0200

    Removed logo.svg
    Svg files can not be used within codesandbox.io

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import logo from '../logo.svg';
 import '../App.css';
 import RomanConverter from './RomanConverter';
 
@@ -7,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Arabic to roman converter</h1>
-        <img src={logo} className="App-logo" alt="logo" />        
         <RomanConverter />
       </header>
     </div >
